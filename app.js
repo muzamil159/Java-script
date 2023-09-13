49664d412857fa584a55783e950de0ab351da058
Muzamil rajpoot <muzamil15945715rajpoot@gmail.com>
2023-09-13 01:46:46 -0700

    Java-script

---
+++ app.js
@@ -0,0 +1,23 @@
+//alert("wellcome")
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        autoplay:true,
+        autoplaytimeout:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+})
